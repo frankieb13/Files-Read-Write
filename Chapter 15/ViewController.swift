55ca59b6fcf958c71972439df0d7970df6b90336
Brad D Messner <bmessner@setonhill.edu>
2020-04-23 13:15:32 -0400

    Initial Commit

---
+++ Chapter 15/ViewController.swift
@@ -0,0 +1,118 @@
+//
+//  ViewController.swift
+//  Chapter 15
+//
+//  Created by Brad D. Messner on 3/20/18.
+//  Copyright © 2018 Brad D. Messner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var drinkPref: UITextField!
+    @IBOutlet weak var results: UITextView?
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func saveResults(_ sender: Any) {
+        let csvLine:String = "\(firstName.text!),\(lastName.text!),\(drinkPref.text!)\n"
+        
+        let paths = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+        let docDir:String=paths[0]
+        let resultsFile:String=(docDir as NSString).appendingPathComponent("results.csv")
+                                                            //bmessner_results.csv
+        
+        
+        
+        // Ended Class 2 here Wed
+        if !FileManager.default.fileExists(atPath: resultsFile) {
+            FileManager.default.createFile(atPath: resultsFile,contents: nil, attributes: nil)
+        }
+        
+        let fileHandle:FileHandle=FileHandle(forUpdatingAtPath:resultsFile)!
+        fileHandle.seekToEndOfFile()
+        fileHandle.write(csvLine.data(using: String.Encoding.utf8)!)
+        fileHandle.closeFile()
+        
+        firstName.text = ""
+        lastName.text = ""
+        drinkPref.text = ""
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func displayResults(_ sender: Any) {
+        let paths = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+        let docDir:String=paths[0] as String
+        
+        let resultsFile:String=(docDir as NSString).appendingPathComponent("results.csv")
+        
+        
+        
+        
+        if FileManager.default.fileExists(atPath: resultsFile) {
+            let fileHandle:FileHandle = FileHandle(forReadingAtPath:resultsFile)!
+            let resultsData:String!=NSString(data: fileHandle.availableData, encoding: String.Encoding.utf8.rawValue)! as String
+            fileHandle.closeFile()
+            
+            
+            results?.text=resultsData
+        }
+        else
+        {
+            // file does not exist
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Chapter 15Tests/Chapter_15Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Chapter_15Tests.swift
+//  Chapter 15Tests
+//
+//  Created by Brad D. Messner on 3/20/18.
+//  Copyright © 2018 Brad D. Messner. All rights reserved.
+//
+
+import XCTest
+@testable import Chapter_15
+
+class Chapter_15Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
